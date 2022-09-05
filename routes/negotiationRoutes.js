cb15525cbcfb279b94ed55f98fbee3875ea62de8
SamuelTheophilus <ansahsamueltheophilus@gmail.com>
2022-09-05 16:37:36 +0000

    changes to the negotiation controllers

--- routes/negotiationRoutes.js
+++ routes/negotiationRoutes.js
@@ -4,10 +4,10 @@ const router = Router();
 const controllers = require('../controllers/negotiationController');
 const constants = require('../constants/constants');
 
-router.get(constants.employee + constants.accept + constants.ID, controllers.employeeAccepted)
-router.get(constants.employee + constants.decline + constants.ID, controllers.employeeDeclined)
-router.get(constants.employee + constants.completed + constants.ID, controllers.employeeCompleted)
-router.get(constants.users + constants.completed + constants.ID, controllers.userAgreement)
+router.put(constants.employee + constants.accept + constants.ID, controllers.employeeAccepted)
+router.put(constants.employee + constants.decline + constants.ID, controllers.employeeDeclined)
+router.put(constants.employee + constants.completed + constants.ID, controllers.employeeCompleted)
+router.put(constants.users + constants.completed + constants.ID, controllers.userAgreement)
 // router.get('/testfalse/:id', controllers.falseSetting);
 
 
