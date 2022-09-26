45902ced572d6fd0ca58c2d1dc847ad74c936841
SamuelTheophilus <ansahsamueltheophilus@gmail.com>
2022-09-26 12:22:42 +0000

    hubtel payment to other numbers

--- controllers/hubtelPaymentControllers.js
+++ controllers/hubtelPaymentControllers.js
@@ -1,5 +1,5 @@
 const Payments = require('../models/paymentModel');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 
 async function userPayment(amount, description, userId, orderId, number) {
@@ -34,16 +34,46 @@ async function userPayment(amount, description, userId, orderId, number) {
 
 }
 
+async function employeePayment(amount,employeeId ) {
 
+  let new_amount = parseFloat(amount)
+
+  async function run() {
+    const mobileNumber = 'YOUR_mobileNumber_PARAMETER';
+    const resp = await fetch(
+      `https://consumer-smrmapi.hubtel.com/send-money/${mobileNumber}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Basic ' + Buffer.from('<username>:<password>').toString('base64')
+        },
+        body: JSON.stringify({
+          amount: new_amount,
+          title: 'HustleUp Payment',
+          description: 'Payment On Completion of Service',
+          clientReference: employeeId,
+          callbackUrl: 'https://hustleup-api.herokuapp.com/payments/employee/success',
+          cancellationUrl: 'https://hustleup-api.herokuapp.com/payments/employee/failure',
+        })
+      }
+    );
+
+    const data = await resp.json();
+    console.log(data);
+  }
+
+  run();
+}
 const receivingMoney = async (req, res) => {
 
   let { description, amount, userId, employeeId, orderId, number } = req.body;
   await userPayment(amount, description, userId, orderId, number);
   let payment = await Payments.create({ description, amount, userId, employeeId });
 
-  if (payment){
+  if (payment) {
     return res.send('Success');
-  }else{
+  } else {
     return res.send('Failure');
   }
 
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -10,9 +10,22 @@ router.post(constants.payments, controller.userPayment)
 router.post(constants.payments + constants.employee, controller.employeePaymentController)
 router.get(constants.payments + constants.paymentlink, controller.getPaylink);
 
-// Paystack callback Routes
+// Hubtel callback Routes
+/** User Payment Callbacks*/
 router.post(constants.payments + constants.success + '/:reference', controller.success)
 router.get(constants.payments + constants.failure, controller.failureNotice)
+
+
+//Employee Payment Callbacks
+router.post('/payments/employee/success', async (req, res) => {
+  console.log('Successful Transaction')
+  res.send('Payment Done');
+})
+
+router.post('/payments/employee/Failure', async (req, res) => {
+  console.log('Failed Transaction')
+  res.send('Payment Not Completed');
+})
 
 
 module.exports = router;
